fcf4ed37c0ff42d7b9ceefda1f7fddb316d6593a
Jia Huang <jia.huang@geneegroup.com>
2014-02-08 06:00:46 +0800

    update gini.sh, move gini core to /usr/local/share/gini-modules

--- data/gini.sh
+++ data/gini.sh
@@ -1,3 +1,3 @@
-export GINI_SYS_PATH="/usr/local/share/gini"
+export GINI_SYS_PATH="/usr/local/share/gini-modules/gini"
 export GINI_MODULE_BASE_PATH="/usr/local/share/gini-modules"
-export PATH="/usr/local/share/gini/bin:$PATH"
+export PATH="/usr/local/share/gini-modules/gini/bin:$PATH"
