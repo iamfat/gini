c3e58b9151de083fb91a6cb85f2bb93438a541e6
Jia Huang <jia.huang@geneegroup.com>
2014-02-10 18:36:28 +0800

    update gini orm json_encode with JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE

--- class/gini/orm.php
+++ class/gini/orm.php
@@ -372,7 +372,7 @@ abstract class ORM
                 $db_data[$k.'_id'] = $o->id ?: 0;
             } elseif (array_key_exists('array', $v)) {
                 $db_data[$k] = isset($this->$k)
-                    ? json_encode($this->$k, true)
+                    ? json_encode($this->$k, true, JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE)
                     : ( array_key_exists('null', $v) ? 'null' : '{}' );
             } else {
                 $db_data[$k] = $this->$k;
