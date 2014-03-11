9b855ebca0498680ed5bb58de1f48486892a58f1
Jia Huang <jia.huang@geneegroup.com>
2014-03-12 04:43:56 +0800

    fix rpc bug

--- class/gini/rpc.php
+++ class/gini/rpc.php
@@ -41,13 +41,13 @@ class RPC
             if (isset($data['error'])) {
                 $message = sprintf('remote error: %s', $data['error']['message']);
                 $code = $data['error']['code'];
-                throw new IoC::construct('\Gini\RPC\Exception', $message, $code);
+                throw IoC::construct('\Gini\RPC\Exception', $message, $code);
             } elseif ($id != $data['id']) {
                 $message = 'wrong response id!';
-                throw new IoC::construct('\Gini\RPC\Exception', $message);
+                throw IoC::construct('\Gini\RPC\Exception', $message);
             } elseif (is_null($data)) {
                 $message = sprintf('unknown error with raw data: %s', $raw_data ?: '(null)');
-                throw new IoC::construct('\Gini\RPC\Exception', $message);
+                throw IoC::construct('\Gini\RPC\Exception', $message);
             }
         }
 
