4fa6e32b6811e5046acc0f2f79867c38077db0bf
Jia Huang <jia.huang@geneegroup.com>
2014-06-10 21:19:47 +0800

    Add authentication mechanism to Gini-Index [skip ci]

--- class/Gini/Controller/CLI/Index.php
+++ class/Gini/Controller/CLI/Index.php
@@ -5,9 +5,50 @@ namespace Gini\Controller\CLI;
 class Index extends \Gini\Controller\CLI
 {
 
-	private function _rcFile()
+	private function _configFile()
 	{
-		return getcwd().'/.ginirc';
+		return getcwd().'/.gini.conf';
+	}
+
+	private function _config()
+	{
+		$configFile = $this->_configFile();
+		if (file_exists($configFile)) {
+	        $config = yaml_parse(@file_get_contents($configFile));
+		}
+
+		return (array) $config;
+	}
+
+	private function _davOptionsAndHeaders()
+	{
+        $uri = $_SERVER['GINI_INDEX_URI'] ?: 'http://gini-index.genee.cn/';
+
+		$config = $this->_config();
+		if (isset($config['token'])) {
+			// Use Token
+            $options = [ 'baseUri' => $uri ];
+            $headers = [ 'Authorization' => 'Gini '.$config['token'] ];
+            
+		} else {
+			// Use Username / Password
+            $userName = readline('User: ');
+            echo 'Password: ';
+            `stty -echo`;
+            $password = rtrim(fgets(STDIN), "\n");
+            `stty echo`;
+            echo "\n";
+
+            $options = [
+                'baseUri' => $uri,
+                'userName' => $userName,
+                'password' => $password,
+            ];
+
+			$headers = [];
+		}
+
+		return [$options, $headers];
 	}
 
 	public function __index($args)
@@ -34,28 +75,26 @@ class Index extends \Gini\Controller\CLI
         `stty echo`;
         echo "\n";
 
-        $config = [
-        	'username' => $username
-        ];
+        $config = [ 'username' => $username ];
         
-        $uri = $_SERVER['GINI_INDEX_URI'] ?: 'http://gini-index.genee.cn/';
-
         try {
+	        $uri = $_SERVER['GINI_INDEX_URI'] ?: 'http://gini-index.genee.cn/';
 	        $rpc = new \Gini\RPC(rtrim($uri, '/').'/api');
 	        $config['token'] = $rpc->createToken($username, $password);
-	        file_put_contents($this->_rcFile(), $config);
+	        file_put_contents($this->_configFile(), $config);
 
 			echo "You've successfully logged in as $username.\n";
         } catch (\Gini\RPC\Exception $e) {
-
+			echo "Server Error: ".$e->getMessage()."\n";
         }
+
 	}
 
 	public function actionLogout($args)
 	{
-		$rc = $this->_rcFile();
-		if (file_exists($rc)) {
-			unlink($rc);
+		$configFile = $this->_configFile();
+		if (file_exists($configFile)) {
+			unlink($configFile);
 		}
 
 		echo "You are logged out now.\n";
@@ -63,17 +102,12 @@ class Index extends \Gini\Controller\CLI
 
 	public function actionWho($args)
 	{
-		$rc = $this->_rcFile();
-		if (file_exists($rc)) {
-	        $content = file_get_contents($rc);
-	        $config = (array) yaml_parse($content);
-		}
-
+		$config = $this->_config();
         if (isset($config['username'])) {
-        	echo "Hey! You are \e[33m".$config['username']."\e[0m!\n";
-        } else {
-        	echo "Oops. You are \e[33mNOBODY\e[0m!\n";
-        }
+    		echo "Hey! You are \e[33m".$config['username']."\e[0m!\n";
+    	} else {
+    		echo "Oops. You are \e[33mNOBODY\e[0m!\n";
+    	}
 	}
 
     public function actionPublish($argv)
@@ -99,35 +133,22 @@ class Index extends \Gini\Controller\CLI
                 die("\e[31mError: $appId/$version missing!\e[0m\n");
             }
 
-            $uri = $_SERVER['GINI_INDEX_URI'] ?: 'http://gini-index.genee.cn/';
-
             // sometimes people will run publish before run composer
             if (!class_exists('\Sabre\DAV\Client')) {
                 require_once SYS_PATH.'/vendor/autoload.php';
             }
 
-            $userName = readline('User: ');
-            echo 'Password: ';
-            `stty -echo`;
-            $password = rtrim(fgets(STDIN), "\n");
-            `stty echo`;
-            echo "\n";
-
-            $options = [
-                'baseUri' => $uri,
-                'userName' => $userName,
-                'password' => $password,
-            ];
+			list($options, $headers) = $this->_davOptionsAndHeaders();
 
             $client = new \Sabre\DAV\Client($options);
-            $response = $client->request('MKCOL', $appId);
+            $response = $client->request('MKCOL', $appId, null, $headers);
             if ($response['statusCode'] == 401 && isset($response['headers']['www-authenticate'])) {
                 // Authentication required
                 // 'Authorization: Basic '. base64_encode("user:password")
                 die ("Failed to publish $appId/$version.\n");
             }
 
-            $response = $client->request('PUT', $path, $content);
+            $response = $client->request('PUT', $path, $content, $headers);
             if ($response['statusCode'] >= 200 && $response['statusCode'] <= 206) {
                 echo "$appId/$version was published successfully.\n";
             } else {
@@ -147,30 +168,17 @@ class Index extends \Gini\Controller\CLI
         $appId = APP_ID;
         $path = "$appId/$version.tgz";
 
-        $uri = $_SERVER['GINI_INDEX_URI'] ?: 'http://gini-index.genee.cn/';
-
         if (!class_exists('\Sabre\DAV\Client')) {
             require_once SYS_PATH.'/vendor/autoload.php';
         }
 
-        $userName = readline('User: ');
-        echo 'Password: ';
-        `stty -echo`;
-        $password = rtrim(fgets(STDIN), "\n");
-        `stty echo`;
-        echo "\n";
-
-        $options = [
-            'baseUri' => $uri,
-            'userName' => $userName,
-            'password' => $password,
-        ];
+        list($options, $headers) = $this->_davOptionsAndHeaders();
 
         $client = new \Sabre\DAV\Client($options);
-        $response = $client->request('HEAD', $path);
+        $response = $client->request('HEAD', $path, null, $headers);
         if ($response['statusCode'] == 200) {
             echo "Unpublishing $appId/$version...\n";
-            $response = $client->request('DELETE', $path);
+            $response = $client->request('DELETE', $path, null, $headers);
             if ($response['statusCode'] >= 200 && $response <= 206) {
                 echo "done.\n";
             } else {
