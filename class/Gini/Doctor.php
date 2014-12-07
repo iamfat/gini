9dabc8b4e7bdb4221232e1a6547f08967c6820d0
Jia Huang <jia.huang@geneegroup.com>
2014-12-07 17:07:56 +0000

    让gini doctor尝试检查每个module的diagnose程序

--- class/Gini/Doctor.php
+++ class/Gini/Doctor.php
@@ -80,6 +80,30 @@ class Doctor
             echo "\n";
         }
 
+        // enumerate all doctor extensions
+        if ((
+                !$items || (
+                    in_array('dependencies', $items) && in_array('module_spec', $items)
+                )
+            ) && !isset($errors['dependencies'])
+        ) {
+            // check gini dependencies
+            foreach (\Gini\Core::$MODULE_INFO as $name => $info) {
+                $class = '\Gini\Module\\'.strtr($name, ['-'=>'', '_'=>'']);
+                $diag_func = "$class::diagnose";
+                if (is_callable($diag_func)) {
+                    echo "Checking Module[$name]...\n";
+                    $module_errors = call_user_func($diag_func);
+                    if ($module_errors) {
+                        static::_outputErrors($module_errors);
+                    } else {
+                        echo "   \e[32mdone.\e[0m\n";
+                    }
+                    echo "\n";
+                }
+            }
+        }
+
         return $errors;
     }
 
