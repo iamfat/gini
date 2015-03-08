31a21526a04b1b8a8fef7cecb1f0d37097487559
Jia Huang <jia.huang@geneegroup.com>
2015-03-08 16:40:43 +0000

    add "gini cgi request <URL> <PARAMS>"

---
+++ class/Gini/Controller/CLI/CGI.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Gini\Controller\CLI;
+
+class CGI extends \Gini\Controller\CLI
+{
+    public function actionRequest($args)
+    {
+        if (count($args) < 1) die("Usage: gini cgi request <URL>\n");
+        $route = array_shift($args);
+
+        array_map(function($arg) {
+            list($k, $v) = explode('=', $arg);
+            $_GET[$k] = $v;
+        }, $args);
+        
+        $env = ['get' => $_GET, 'post' => $_POST, 'files' => $_FILES, 'route' => $route ];
+
+        echo "Requesting {$route}...\n";
+        $content = \Gini\CGI::request($route, $env)->execute()->content();
+        if (is_string($content)) {
+            echo $content;
+        } else {
+            echo yaml_emit($content);
+        }
+    }
+
+}
