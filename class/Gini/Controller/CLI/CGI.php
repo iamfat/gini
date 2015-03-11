19256f2be4d91b33fa0f100d9f15bed059723967
Jia Huang <jia.huang@geneegroup.com>
2015-03-11 09:05:11 +0000

    update `gini cgi request` format

--- class/Gini/Controller/CLI/CGI.php
+++ class/Gini/Controller/CLI/CGI.php
@@ -6,14 +6,16 @@ class CGI extends \Gini\Controller\CLI
 {
     public function actionRequest($args)
     {
-        if (count($args) < 1) die("Usage: gini cgi request <URL>\n");
+        if (count($args) < 1) {
+            die("Usage: gini cgi request <URL>\n");
+        }
         $route = array_shift($args);
 
-        array_map(function($arg) {
+        array_map(function ($arg) {
             list($k, $v) = explode('=', $arg);
             $_GET[$k] = $v;
         }, $args);
-        
+
         $env = ['get' => $_GET, 'post' => $_POST, 'files' => $_FILES, 'route' => $route ];
 
         echo "Requesting {$route}...\n";
@@ -24,5 +26,4 @@ class CGI extends \Gini\Controller\CLI
             echo yaml_emit($content);
         }
     }
-
 }
