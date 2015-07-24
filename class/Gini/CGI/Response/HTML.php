052a4ce0b3e49860885eb7991d27977505cca458
pihizi <pihizi@msn.com>
2015-07-24 14:38:49 +0800

    HTML支持文件编码

--- class/Gini/CGI/Response/HTML.php
+++ class/Gini/CGI/Response/HTML.php
@@ -5,15 +5,17 @@ namespace Gini\CGI\Response;
 class HTML implements \Gini\CGI\Response
 {
     private $_content;
+    private $_charset;
 
-    public function __construct($content)
+    public function __construct($content, $charset='utf-8')
     {
         $this->_content = $content;
+        $this->_charset = $charset;
     }
 
     public function output()
     {
-        header('Content-Type: text/html');
+        header('Content-Type: text/html; charset:' . $this->_charset);
         file_put_contents('php://output', (string) $this->_content);
     }
 
