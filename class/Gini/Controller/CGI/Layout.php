281521196b26d9850e59115d583de4fcc2cb7e84
Jia Huang <jia.huang@geneegroup.com>
2016-06-15 17:18:50 +0800

    fix __preAction and __postAction function type in Layout

--- class/Gini/Controller/CGI/Layout.php
+++ class/Gini/Controller/CGI/Layout.php
@@ -7,14 +7,14 @@ abstract class Layout extends \Gini\Controller\CGI
     public $view;
     protected static $layout_name = 'layout';
 
-    public function __preAction($action, &$params)
+    protected function __preAction($action, &$params)
     {
         parent::__preAction($action, $params);
         $this->view = V(static::$layout_name);
         $this->view->title = \Gini\Config::get('layout.title');
     }
 
-    public function __postAction($action, &$params, $response)
+    protected function __postAction($action, &$params, $response)
     {
         parent::__postAction($action, $params, $response);
         if (null === $response) {
