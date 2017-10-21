iamfat/gini

commit e0a175259f28909839455e3c9e0386f80c2f070b

jsonrpc API support named parameters

class/Gini/API.php
@@ -51,7 +51,7 @@ class API
                     $method = 'action'.$method;
                     $o = \Gini\IoC::construct($class);
                     if (method_exists($o, $method)) {
-                        $callback = array($o, $method);
+                        $callback = [$o, $method];
                     } elseif (function_exists($class.'\\'.$method)) {
                         $callback = $class.'\\'.$method;
                     }
@@ -62,7 +62,27 @@ class API
                 throw new API\Exception('Method not found', -32601);
             }
 
-            $result = call_user_func_array($callback, $params);
+            if (is_string($callback)) {
+                $r = new \ReflectionFunction($callback);
+            } else {
+                $r = new \ReflectionMethod($callback[0], $callback[1]);
+            }
+            if (is_numeric(key($params))) {
+                // 使用array_pad确保不会因为变量没有默认设值而报错
+                $args = array_pad($params, $r->getNumberOfParameters(), null);
+            } else {
+                // 如果是有字符串键值的, 尝试通过反射对应变量
+                $rps = $r->getParameters();
+                $args = [];
+                // 可以把form数据合并进去
+                $params = array_merge((array)$params, (array)$form);
+                foreach ($rps as $rp) {
+                    $args[] = $params[$rp->name] ?:
+                        ($rp->isDefaultValueAvailable() ? $rp->getDefaultValue() : null);
+                }
+            }
+    
+            $result = call_user_func_array($callback, $args);
 
             if ($id !== null) {
                 $response = [

class/Gini/RPC.php
@@ -37,12 +37,15 @@ class RPC
         return IoC::construct('\Gini\RPC', $this->_url, $this->_path ? $this->_path.'/'.$name : $name, $this->_cookie, $this->_header);
     }
 
-    public function __call($method, $params)
+    /**
+     * RPC call with named parameters
+     *
+     * @param [string] $method
+     * @param [array] $params
+     * @return [mixed]
+     */
+    public function call($method, $params)
     {
-        if ($method === __FUNCTION__) {
-            return;
-        }
-
         if ($this->_path) {
             $method = $this->_path.'/'.$method;
         }
@@ -79,6 +82,15 @@ class RPC
         }
 
         return $data['result'];
+    }
+
+    public function __call($method, $params)
+    {
+        if ($method === __FUNCTION__) {
+            return;
+        }
+
+        return $this->call($method, $params);
     }
 
     public function setHeader(array $header)

tests/unit/RPC/General.php
@@ -6,17 +6,7 @@ namespace Gini\PHPUnit\RPC {
 
     class General extends \Gini\PHPUnit\CLI
     {
-        private function _rpcCall($method, $params)
-        {
-            return [
-                    'jsonrpc' => '2.0',
-                    'method' => $method,
-                    'params' => $params,
-                    'id' => uniqid(),
-                ];
-        }
-
-        private function _testCall($method, $params)
+       private function _testCall($method, $params)
         {
             $apiData =  [
                     'jsonrpc' => '2.0',
@@ -25,17 +15,32 @@ namespace Gini\PHPUnit\RPC {
                     'id' => uniqid(),
                 ];
 
-            $response = \Gini\API::dispatch($apiData);
-            $this->assertSame($params[0], $response['result'], 'call '.$method);
+            return \Gini\API::dispatch($apiData);
         }
 
         public function testApiName()
         {
             $var = uniqid();
 
-            $this->_testCall('RPCTest.camelCaseMethod', [$var]);
+            $response = $this->_testCall('RPCTest.camelCaseMethod', [$var]);
+            $this->assertSame($var, $response['result'], 'call RPCTest.camelCaseMethod');
+
             $this->_testCall('rpctest.camelcasemethod', [$var]);
+            $this->assertSame($var, $response['result'], 'call rpctest.camelcasemethod');
+
             $this->_testCall('rpctest/camelCaseMethod', [$var]);
+            $this->assertSame($var, $response['result'], 'rpctest/camelCaseMethod');
+        }
+
+        public function testNamedParameters()
+        {
+            $var = uniqid();
+
+            $response = $this->_testCall('RPCTest.echo', ['a' => 1, 'b' => 2, 'c' => 3]);
+            $this->assertSame($response['result'], ['a' => 1, 'b' => 2, 'c' => 3]);
+
+            $response = $this->_testCall('RPCTest.echo', ['b' => 2, 'a' => 1, 'c'=> 3]);
+            $this->assertSame($response['result'], ['a' => 1, 'b' => 2, 'c' => 3]);
         }
     }
 
@@ -48,6 +53,10 @@ namespace Gini\Controller\API {
         public function actionCamelCaseMethod($s)
         {
             return $s;
+        }
+
+        public function actionEcho($a, $b, $c) {
+            return ['a' => $a, 'b' => $b, 'c' => $c];
         }
     }
 

commit 4c6760461da40147607ddb243cf6a4647aba81f6

fix parameter default value bug caused by commit e0a175259f28909839455e3c9e0386f80c2f070b

class/Gini/API.php
@@ -67,13 +67,18 @@ class API
             } else {
                 $r = new \ReflectionMethod($callback[0], $callback[1]);
             }
+
+            $args = [];
+            $rps = $r->getParameters();
             if (is_numeric(key($params))) {
                 // 使用array_pad确保不会因为变量没有默认设值而报错
-                $args = array_pad($params, $r->getNumberOfParameters(), null);
+                // 但是需要考虑默认
+                foreach ($rps as $idx => $rp) {
+                    $args[] = $params[$idx] ?:
+                        ($rp->isDefaultValueAvailable() ? $rp->getDefaultValue() : null);
+                }
             } else {
                 // 如果是有字符串键值的, 尝试通过反射对应变量
-                $rps = $r->getParameters();
-                $args = [];
                 // 可以把form数据合并进去
                 $params = array_merge((array)$params, (array)$form);
                 foreach ($rps as $rp) {

class/Gini/CGI.php
@@ -159,14 +159,18 @@ class CGI
 
     public static function executeAction($action, $params, $form=null)
     {
-        $rm = new \ReflectionMethod($action[0], $action[1]);
+        $args = [];
+        $r = new \ReflectionMethod($action[0], $action[1]);
+        $rps = $r->getParameters();
         if (is_numeric(key($params))) {
             // 使用array_pad确保不会因为变量没有默认设值而报错
-            $args = array_pad($params, $rm->getNumberOfParameters(), null);
+            // 但是需要考虑默认
+            foreach ($rps as $idx => $rp) {
+                $args[] = $params[$idx] ?:
+                    ($rp->isDefaultValueAvailable() ? $rp->getDefaultValue() : null);
+            }
         } else {
             // 如果是有字符串键值的, 尝试通过反射对应变量
-            $rps = $rm->getParameters();
-            $args = [];
             // 可以把form数据合并进去
             $params = array_merge((array)$params, (array)$form);
             foreach ($rps as $rp) {