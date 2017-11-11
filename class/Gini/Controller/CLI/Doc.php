fca2a95783eac54969e041a271bb90f7ea266c97
Jia Huang <iamfat@gmail.com>
2017-11-11 18:59:51 +0800

    add Gini\Document framework, list api/cgi/cli/rest easily

---
+++ class/Gini/Controller/CLI/Doc.php
@@ -0,0 +1,106 @@
+<?php
+
+namespace Gini\Controller\CLI;
+
+class Doc extends \Gini\Controller\CLI
+{
+    public function actionAPI($args)
+    {
+        $doc = \Gini\Document::of('Gini\\Controller\\API')->filterClasses(function ($rc) {
+            return !$rc->isAbstract() && !$rc->isTrait() && !$rc->isInterface();
+        })->filterMethods(function ($rm) {
+            return $rm->isPublic() && !$rm->isConstructor()
+            && !$rm->isDestructor()
+            && preg_match('/^action/', $rm->name);
+        })->format();
+    }
+
+    public function actionCLI($args)
+    {
+        $doc = \Gini\Document::of('Gini\\Controller\\CLI')->filterClasses(function ($rc) {
+            return !$rc->isAbstract() && !$rc->isTrait() && !$rc->isInterface();
+        })->filterMethods(function ($rm) {
+            return $rm->isPublic() && !$rm->isConstructor()
+            && !$rm->isDestructor()
+            && preg_match('/^action/', $rm->name);
+        })->format(function ($unit) {
+            $class = preg_replace('/^'.preg_quote('\\Gini\\Controller\\CLI\\').'/', '', $unit->class);
+            $classUnits = explode('\\', $class);
+            $classUnits = array_map(function ($u) {
+                return strtolower(preg_replace('/([a-z])([A-Z])/', '$1-$2', $u));
+            }, $classUnits);
+
+            $args = array_merge(['gini'], $classUnits);
+            $args[] = strtolower(preg_replace('/([a-z])([A-Z])/', '$1-$2', preg_replace('/^action/', '', $unit->method)));
+            echo implode(' ', $args)."\n";
+        });
+    }
+
+    public function actionCGI($args)
+    {
+        $doc = \Gini\Document::of('Gini\\Controller\\CGI')->filterClasses(function ($rc) {
+            return !$rc->isAbstract() && !$rc->isTrait() && !$rc->isInterface();
+        })->filterMethods(function ($rm) {
+            return $rm->isPublic() && !$rm->isConstructor()
+            && !$rm->isDestructor()
+            && ($rm->name == '__index' || preg_match('/^action/', $rm->name));
+        })->format(function ($unit) {
+            $class = preg_replace('/^'.preg_quote('\\Gini\\Controller\\CGI\\').'/', '', $unit->class);
+            $pathUnits = explode('\\', $class);
+            $pathUnits = array_map(function ($u) {
+                return strtolower(preg_replace('/([a-z])([A-Z])/', '$1-$2', $u));
+            }, $pathUnits);
+
+            if ($unit->method !== '__index') {
+                $pathUnits[] = strtolower(preg_replace('/([a-z])([A-Z])/', '$1-$2', preg_replace('/^action/', '', $unit->method)));
+            }
+
+            if (count($unit->params) > 0) {
+                foreach ($unit->params as $param) {
+                    $docParam = strtolower(preg_replace('/([a-z])([A-Z])/', '$1-$2', strtr($param['name'], '_', '-')));
+                    if (isset($param['default'])) {
+                        $docParam .= '=' . strval($param['default']);
+                    }
+                    $pathUnits[] = '{'.$docParam.'}';
+                }
+            }
+
+            echo 'REQUEST ' . implode('/', $pathUnits)."\n";
+        });
+    }
+
+    public function actionREST($args)
+    {
+        $doc = \Gini\Document::of('Gini\\Controller\\CGI')->filterClasses(function ($rc) {
+            return !$rc->isAbstract() && !$rc->isTrait() && !$rc->isInterface();
+        })->filterMethods(function ($rm) {
+            return $rm->isPublic() && !$rm->isConstructor()
+            && !$rm->isDestructor()
+            && preg_match('/^(get|post|delete|put|options)/', $rm->name);
+        })->format(function ($unit) {
+            $class = preg_replace('/^'.preg_quote('\\Gini\\Controller\\CGI\\').'/', '', $unit->class);
+            $pathUnits = explode('\\', $class);
+            $pathUnits = array_map(function ($u) {
+                return strtolower(preg_replace('/([a-z])([A-Z])/', '$1-$2', $u));
+            }, $pathUnits);
+
+            if (preg_match('/^(get|post|delete|put|options)(.+)$/', $unit->method, $matches)) {
+                $method = strtoupper($matches[1]);
+                $pathUnits[] = strtolower(preg_replace('/([a-z])([A-Z])/', '$1-$2', $matches[2]));
+                echo $method.' '.implode('/', $pathUnits);
+                if (count($unit->params) > 0) {
+                    $docParams = [];
+                    foreach ($unit->params as $param) {
+                        $docParam = $param['name'];
+                        if (isset($param['default'])) {
+                            $docParam .= ': ' . J($param['default']);
+                        }
+                        $docParams[] = $docParam;
+                    }
+                    echo "\t{ ".implode(', ', $docParams)." }";
+                }
+                echo "\n";
+            }
+        });
+    }
+}
---
+++ class/Gini/Document.php
@@ -0,0 +1,119 @@
+<?php
+
+namespace Gini;
+
+class Document
+{
+    private $_baseClass;
+    private $_fileFilters = [];
+    private $_classFilters = [];
+    private $_methodFilters = [];
+
+    public static function of($baseClass)
+    {
+        return IoC::construct('\Gini\Document', $baseClass);
+    }
+
+    public function __construct($baseClass='Gini')
+    {
+        $this->_baseClass = $baseClass;
+    }
+
+    public function filterFiles($func)
+    {
+        $this->_fileFilters[] = $func;
+        return $this;
+    }
+
+    public function filterClasses($func)
+    {
+        $this->_classFilters[] = $func;
+        return $this;
+    }
+
+    public function filterMethods($func)
+    {
+        $this->_methodFilters[] = $func;
+        return $this;
+    }
+
+    public function format($formatFunc=null)
+    {
+        $baseClass = trim($this->_baseClass, '\\');
+        $baseDir = str_replace('\\', '/', $baseClass);
+        $dirs = Core::pharFilePaths(CLASS_DIR, $baseDir);
+        foreach ($dirs as $dir) {
+            if (!is_dir($dir)) {
+                continue;
+            }
+
+            File::eachFilesIn($dir, function ($file) use ($baseClass, &$formatFunc) {
+                if (File::extension($file) != 'php') {
+                    return;
+                }
+
+                foreach ($this->_fileFilters as $func) {
+                    if (!$func($file)) {
+                        return;
+                    }
+                }
+
+                $name = preg_replace('|.php$|', '', $file);
+                $className = '\\' . $baseClass . '\\' . str_replace('/', '\\', $name);
+
+                // Check if it is abstract class
+                $rc = new \ReflectionClass($className);
+                foreach ($this->_fileFilters as $func) {
+                    if (false === $func($rc)) {
+                        return;
+                    }
+                }
+
+                $docClassName = strtr($name, ['/'=>'.']);
+                $rms = $rc->getMethods();
+                foreach ($rms as $rm) {
+                    $skip = false;
+                    foreach ($this->_methodFilters as $func) {
+                        if (false === $func($rm)) {
+                            $skip = true;
+                            break;
+                        }
+                    }
+                    if ($skip) {
+                        continue;
+                    }
+
+                    // $method = strtolower($method[0]).substr($method, 1);
+                    $rps = $rm->getParameters();
+                    // echo $rm->getDocComment()."\n";
+                    $method = preg_replace('/^(action|get|post|delete|put|options)/', '', $rm->name);
+                    $docMethod = "$docClassName.$method";
+                    if ($formatFunc) {
+                        $params = array_map(function ($rp) {
+                            $param = ['name' => $rp->name];
+                            $rp->hasType() and $param['type'] = $rp->getType();
+                            $rp->isPassedByReference() and $param['ref'] = true;
+                            $rp->isDefaultValueAvailable() and $param['default'] = $rp->getDefaultValue();
+                            return $param;
+                        }, $rps);
+                        $formatFunc((object) [
+                            'class' => $className,
+                            'method' => $rm->name,
+                            'params' => $params,
+                        ]);
+                    } else {
+                        $docParams = array_map(function ($rp) {
+                            $decl = '';
+                            $rp->hasType() and $decl .= $rp->getType() . ' ';
+                            $rp->isPassedByReference() and $decl .= '&';
+                            $decl .= '$'.$rp->name;
+                            $rp->isDefaultValueAvailable() and $decl .= '='.J($rp->getDefaultValue());
+                            return $decl;
+                        }, $rps);
+                        echo $docMethod . "(" . implode(', ', $docParams) .")\n";
+                    }
+                }
+            });
+        }
+    }
+}
