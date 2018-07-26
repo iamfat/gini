d028bee207cf5e024d55a1b308573c19cc581f8c
txw <txwyates@gmail.com>
2018-07-26 13:26:33 +0800

    确保解析的时候只要遇到第一个分号中止即可

--- class/Gini/CGI.php
+++ class/Gini/CGI.php
@@ -202,7 +202,7 @@ class CGI
         static::$route = trim($_SERVER['PATH_INFO'] ?: $_SERVER['ORIG_PATH_INFO'], '/');
         Session::setup();
 
-        if (explode(';', $_SERVER['CONTENT_TYPE'])[0] == 'application/json') {
+        if (explode(';', $_SERVER['CONTENT_TYPE'], 2)[0] == 'application/json') {
             $_POST = (array) @json_decode(self::content(), true);
         } elseif (!in_array($_SERVER['REQUEST_METHOD'], ['GET', 'POST'])) {
             @parse_str(self::content(), $_POST);
