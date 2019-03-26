b3ba36fbe0710b19e53bc1e25b99fffce0e3aa7c
Jia Huang <iamfat@gmail.com>
2019-03-26 17:47:01 +0800

    use ORM/Base to replace ORM/Object for PHP 7.2 compatibility

--- class/Gini/ORM/Object.php
+++ class/Gini/ORM/Object.php
@@ -2,41 +2,7 @@
 
 namespace Gini\ORM;
 
-abstract class Object extends \Gini\ORM
+// Deprecated since PHP 7.2
+abstract class Object extends Base
 {
-    public $id = 'bigint,primary,serial';
-    public $_extra = 'array';
-
-    protected static $db_name;      // database name
-    protected static $db_index;     // database index
-    protected static $db_relation;  // database relation
-
-    private $those;    // add to support those API
-
-    public function fetch($force = false)
-    {
-        if ($this->criteria() === null && $this->those) {
-            //try those API
-            $ids = (array) $this->those->limit(1)->get('id');
-            $this->criteria(reset($ids));
-        }
-
-        return parent::fetch($force);
-    }
-
-    public function __call($method, $params)
-    {
-        if ($method == __CLASS__) {
-            return;
-        }
-
-        if (method_exists('\Gini\Those', $method)) {
-            if (!$this->those) {
-                $this->those = \Gini\IoC::construct('\Gini\Those', $this->name());
-            }
-            call_user_func_array([$this->those, $method], $params);
-
-            return $this;
-        }
-    }
 }
